ead1fba8ff38e4646f12f6511c8056087bfc4dfd
iotktv <wahyudi.prasatia@koltiva.com>
2024-03-05 11:33:29 +0700

    Updates

---
+++ precache-manifest.90e1191a8f4532d538de32ccf542f4cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8211b8449e10364ce5a30abb7d55ba02",
+    "url": "/ota-ble-web/index.html"
+  },
+  {
+    "revision": "d9e150c50df365d8a65c",
+    "url": "/ota-ble-web/static/css/main.7e6de23a.chunk.css"
+  },
+  {
+    "revision": "63434a638cabb531d86c",
+    "url": "/ota-ble-web/static/js/2.ddd56c7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ota-ble-web/static/js/2.ddd56c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e150c50df365d8a65c",
+    "url": "/ota-ble-web/static/js/main.7517375e.chunk.js"
+  },
+  {
+    "revision": "eb6c4d1ac783253f271c",
+    "url": "/ota-ble-web/static/js/runtime-main.22f7c7f0.js"
+  }
+]);
